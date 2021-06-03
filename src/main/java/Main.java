f5e1a45b07f1789524e4ecabc068f16a5292aeb5
andrei88M <andrei1988mikhalchuk@gmail.com>
2021-06-03 19:02:05 +0300

    !

--- src/main/java/Main.java
+++
@@ -1,4 +0,0 @@
-public class Main {
-/*В предыдущем проекте обращение к базе сделать через hibernate (Session).
-Тесты, jacoco, checkstyle подключить тоже.*/
-}
---
+++ src/main/java/by/it/Main.java
@@ -0,0 +1,5 @@
+package by.it;
+
+public class Main {
+    //new task
+}
