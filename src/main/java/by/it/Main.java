4ad909c5989d8e8bbc73e96bc871ef1d25ce6c0a
andrei88M <andrei1988mikhalchuk@gmail.com>
2021-06-06 23:36:44 +0300

    new task

--- src/main/java/by/it/Main.java
+++ src/main/java/by/it/Main.java
@@ -1,5 +1,25 @@
 package by.it;
 
 public class Main {
-    //25.05
+    /*Создать три класса
+1) Task - имеет следующие поля
+- id
+- name
+- description
+
+2) HomeTask exstends Task - имеет дополнительные поля
+- startDate
+- endDate
+    @Embedded
+- Address address (address имеет два поля street, city сделать Embeddable)
+
+3) WorkTask exstends Task - имеет дополнительные поля
+- cost
+
+    В трех ветках git реализовать 3 стратегии наследования (16 перзентация)
+1 - Table per class hierarchy
+2 - Table per subclass
+3 - Table Per Concrete class mapping
+
+Записать туда какие нить данные из main*/
 }
